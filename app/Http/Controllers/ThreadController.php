40b307daadc0a03374323c67b3393645bc2299a7
Josh Benham <joshbenham@gmail.com>
2018-01-10 15:48:55 +1100

    Lesson 2 - Test-Driving Threads

--- app/Http/Controllers/ThreadController.php
+++ app/Http/Controllers/ThreadController.php
@@ -14,7 +14,8 @@ class ThreadController extends Controller
      */
     public function index()
     {
-        //
+        $threads = Thread::latest()->get();
+        return view('thread.index', compact('threads'));
     }
 
     /**
@@ -46,7 +47,7 @@ class ThreadController extends Controller
      */
     public function show(Thread $thread)
     {
-        //
+        return view('thread.show', compact('thread'));
     }
 
     /**
