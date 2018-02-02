91040c279d3e9a05476cc718ca9ed4f8d1841fd8
Josh Benham <joshbenham@gmail.com>
2018-02-02 16:47:35 +1100

    Lesson 57 - Mentioned Users Notifications Part 2

---
+++ app/Listeners/NotifyMentionedUsers.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\ThreadReceivedNewReply;
+use App\User;
+use App\Notifications\YouWereMentioned;
+
+class NotifyMentionedUsers
+{
+    /**
+     * Handle the event.
+     *
+     * @param  ThreadReceivedNewReply  $event
+     * @return void
+     */
+    public function handle(ThreadReceivedNewReply $event)
+    {
+        $mentionedUsers = $event->reply->mentionedUsers();
+
+        collect($event->reply->mentionedUsers())
+            ->map(function ($name) {
+                return User::whereName($name)->first();
+            })
+            ->filter()
+            ->each(function ($user) use ($event) {
+                $user->notify(new YouWereMentioned($event->reply));
+            });
+    }
+}
--- app/Thread.php
+++ app/Thread.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Events\ThreadReceivedNewReply;
 
 class Thread extends Model
 {
@@ -52,17 +53,9 @@ class Thread extends Model
     {
         $reply = $this->replies()->create($reply);
 
-        $this->notifySubscribers($reply);
+        event(new ThreadReceivedNewReply($reply));
 
         return $reply;
-    }
-
-    public function notifySubscribers($reply)
-    {
-        $this->subscriptions
-            ->where('user_id', '!=', $reply->user_id)
-            ->each
-            ->notify($reply);
     }
 
     public function subscribe($userId = null)
