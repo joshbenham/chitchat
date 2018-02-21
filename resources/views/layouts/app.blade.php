cb0ea1d537e4e5767d894033776befb62e6737dc
Josh Benham <joshbenham@gmail.com>
2018-02-22 10:56:50 +1100

    Lesson 92 - A Thread Can Be Updated - Part 2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,6 +24,7 @@
     <style>
         body { padding-bottom: 100px; }
         .level { display: flex; align-items: center; }
+        .level-item { margin-right: 1em; }
         .flex { flex: 1; }
         .ml-a { margin-left: auto; }
         .mr-1 { margin-right: 1em; }
